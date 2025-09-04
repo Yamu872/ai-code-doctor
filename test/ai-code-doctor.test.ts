042472e6d04bbeb2f558017a95ffe49d48f1c7f3
Yamu872 <tatsuya.yamamoto@serverworks.co.jp>
2025-09-04 10:48:30 +0900

    Initial commit

---
+++ test/ai-code-doctor.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AiCodeDoctor from '../lib/ai-code-doctor-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ai-code-doctor-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AiCodeDoctor.AiCodeDoctorStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
